e756ec3925ffdb650ac43cc6b3873ffe9682dbb6
Brett Derham <bderham565@gmail.com>
2018-01-16 13:45:36 -0800

    Added Javadocs basics

--- homework00/HelloWorld.java
+++ homework00/HelloWorld.java
@@ -1,3 +1,22 @@
+/** @(#)HelloWorld.java   1.0.1   16-Jan-2018
+    *  Purpose    : To print the string "Hello, world!"
+    *  Author     : Brett Derham
+    *  Description: This file provides a simple class file 
+    *  that prints out the string "Hello, world!" 
+    *  in the terminal.      
+    */
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * Revision History:
+    * -----------------
+    *
+    *  Ver      Date      Modified by:  Description of change/modification
+    * -----  -----------  ------------  --------------------------------------
+    * 1.0.0  11-Jan-2018  B. Derham     Created original file 
+    * 1.0.1  16-Jan-2018  B. Derham     Added file name, date, version, 
+    *                                   description, and revision history
+    * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 public class HelloWorld {
   public static void main( String[] args ) {
       System.out.println( "Hello, world!" );
